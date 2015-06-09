fcaeb8de72a840d8155a70089482b8e5a4ecaa9e
Brian E Clow <bclow@invoca.com>
2015-06-09 10:45:04 -0700

    metadata.rb updates
     - Updating docker version
     - Sorting deps
     - Adding yum as a dep
     - Updating cookbook comment

--- metadata.rb
+++ metadata.rb
@@ -2,16 +2,12 @@ name             'jenkins-ci'
 maintainer       'Brian E Clow'
 maintainer_email 'bclow-github@temporalflux.org'
 license          'Apache 2.0'
-description      'Cookbook to demonstrate using Jenkins for Chef Cookbook CI'
+description      'Cookbook to ease the use of Jenkins for Chef Cookbook CI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-# We'll be needing jenkins
 depends 'apt'
-depends 'jenkins', '~> 2.2.2'
-
-# We'll be pulling cookbooks using git
+depends 'docker', '~> 0.37.0'
 depends 'git', '~> 4.1.0'
-
-# We'll run integration tests using docker
-depends 'docker', '~> 0.36.0'
+depends 'jenkins', '~> 2.2.2'
+depends 'yum'
